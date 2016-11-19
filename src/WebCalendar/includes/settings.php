c0d381a79d5368e474851a30d84ef7b866e2b450
Panghao <PH@PanghaodeMacBook-Pro.local>
2016-11-19 18:48:42 +1100

    version1.1

---
+++ src/WebCalendar/includes/settings.php
@@ -0,0 +1,16 @@
+<?php
+/* updated via install/index.php on Tue, 01 Nov 2016 01:34:43 -0400
+install_password: 36dbff3bd682e3935b5f788e7b4f7d04
+db_type: mysql
+db_host: localhost:3306
+db_database: WebCalendar1234
+db_login: web1234
+db_password: greencity
+db_persistent: false
+readonly: false
+user_inc: user.php
+use_http_auth: false
+single_user: false
+db_cachedir: /tmp
+# end settings.php */
+?>
